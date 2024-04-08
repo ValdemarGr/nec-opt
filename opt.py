c54cf089d05807de246f3be8f652ce30662c8f8e
Valdemar Grange <randomvald0069@gmail.com>
2024-04-08 17:57:47 +0200

    wok

--- opt.py
+++ opt.py
@@ -5,18 +5,25 @@ m = [
     {0,1,2,3,4,16,15,14,13,12,11,10},
     {3,4,5,6,11,15,14},
     {0,1,2,3,4,5,6,11,14,15,16},
-    {0,1,2,3,5,6,7,11,12,13,14,15,16},
-    {0,1,2,6,7,8,9,10,11,12,15,16},
-    {0,1,2,3,6,7,8,9,10,11,12,15,16},
-    {0,1,2,3,4,5,6,7,10,11,12,13,14,15,16}
+    {},
+    {},
+    {},
+    {},
+    # {0,1,2,3,5,6,7,11,12,13,14,15,16},
+    # {0,1,2,6,7,8,9,10,11,12,15,16},
+    # {0,1,2,3,6,7,8,9,10,11,12,15,16},
+    # {0,1,2,3,4,5,6,7,10,11,12,13,14,15,16}
 ]
 
 # we have a 3d matrix of 17 * 8 * 3 (for three choices)
 
-CLEAR = 0
-ADJ = 1
-ROW = 2
-COL = 3
+ADJ = 0
+ROW = 1
+COL = 2
+
+MODS = range(3)
+COLUMNS = range(17)
+ROWS = range(8)
 
 # A[i, k, k] is binary
 
@@ -33,17 +40,22 @@ COL = 3
 
 model = pyomo.ConcreteModel()
 
-model.A = pyomo.Var(range(17), range(8), range(4), domain=pyomo.Binary)
+model.A = pyomo.Var(COLUMNS, ROWS, MODS, domain=pyomo.Binary)
+
+CELLS = [(c, r) for c in COLUMNS for r in ROWS]
+
+ACTIVE_CELLS = [(c, r) for r, cols in enumerate(m) for c in cols]
 
 model.cons = pyomo.ConstraintList()
 
-for c in range(17):
-    for r in range(8):
-        n = 1 if c in m[r] else 0
-        model.cons.add(sum(model.A[c, r, k] for k in range(4)) == n)
+for c, r in CELLS:
+    if c in m[r]:
+        model.cons.add(sum(model.A[c, r, k] for k in MODS) <= 1)
+    else:
+        model.cons.add(sum(model.A[c, r, k] for k in MODS) == 0)
 
 def safe_get(c, r, k):
-    if c < 0 or 16 < c or r < 0 or 7 < r:
+    if c not in COLUMNS or r not in ROWS:
         return 0
     return model.A[c, r, k]
 
@@ -70,41 +82,54 @@ def connectedVertical(c, r):
   down = connected(rows, r+1, lambda x: x + 1)
   return up + down
 
-def multiplier(c, r):
-  return (safe_get(c-1, r, ADJ) + safe_get(c+1, r, ADJ) + safe_get(c, r-1, ADJ) + safe_get(c, r+1, ADJ)) * 1.4 + sum(model.A[x, r, ROW] for x in connectedHorizontal(c, r)) * 1.25 + sum(model.A[c, x, COL] for x in connectedVertical(c, r)) * 1.25
+def adj(c, r): 
+  return (safe_get(c-1, r, ADJ) + safe_get(c+1, r, ADJ) + safe_get(c, r-1, ADJ) + safe_get(c, r+1, ADJ)) * 0.4
 
-modifiers = sum(
-  sum(
-    multiplier(c, r) * model.A[c, r, CLEAR]
-    for r in range(8)
-  )
-  for c in range(17)
-)
+def row(c, r):
+  print("row", c, r, connectedHorizontal(c, r))
+  return sum(model.A[x, r, ROW] for x in connectedHorizontal(c, r)) * 0.25
+
+def col(c, r):
+  print("col", c, r, connectedVertical(c, r))
+  return sum(model.A[c, x, COL] for x in connectedVertical(c, r)) * 0.25
+
+def multiplier(c, r):
+  return 1 + adj(c, r) + row(c, r) + col(c, r)
+
+# big M variable
+M = 2
+model.w = pyomo.Var(COLUMNS, ROWS, domain=pyomo.NonNegativeReals)
+
+for c, r in ACTIVE_CELLS:
+  model.cons.add(model.w[c, r] <= multiplier(c, r))
+  model.cons.add(model.w[c, r] <= M * (1 - model.A[c, r, ADJ] - model.A[c, r, ROW] - model.A[c, r, COL]))
 
 model.obj = pyomo.Objective(
-  expr = modifiers,
+  expr = sum(model.w[c, r] for c, r in ACTIVE_CELLS),
   sense=pyomo.maximize
 )
 
-print(sum(len(x) for x in m))
+solver = pyomo.SolverFactory("cbc")
+model.pprint()
+solution = solver.solve(model, tee=True)
 
-solver = pyomo.SolverFactory("mindtpy")
-solution = solver.solve(model, tee=True, mip_solver='cbc', nlp_solver='ipopt')
+#print(solution)
 
-print(solution)
-
-for r in range(8):
+for r in ROWS:
   s = ""
-  for c in range(17):
+  for c in COLUMNS:
     sub = ""
     if c in m[r]:
-      for k in range(4):
+      for k in MODS:
         if model.A[c, r, k].value == 1:
-          sub = "CAR0"[k]
+          sub = "ARC"[k]
+
+      if sub == "":
+        sub = "N"
     else:
       sub = "E"
 
     s += sub + " | "
   print(s)
 
-print("Objective: ", modifiers())
+#print("Objective: ", modifiers())
